e897133bdbe41daa86b9d7430c912badeb0c9bb2
Kayeon <kky1373@naver.com>
2022-12-02 00:56:48 +0900

    Events part Two

--- web.js
+++ web.js
@@ -11,6 +11,13 @@ function onLoginSubmit(event){
     
 }
 loginForm.addEventListener("submit",onLoginSubmit);
+/////////////////////
 
-
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event) // 사용자가 클릭한 부분에 대한 정보를 알려줌(x축, y축 , html...)
+}
 const link= document.querySelector("a");
+link.addEventListener("click",handleLinkClick)
+// ok 누르기 전까지는 링크로 이동 X(alert 사용하면 브라우저 모든 행동이 창 끌때까지 멈춤)
+// 뭐가 클릭 됐는지 정보 알고 싶음
