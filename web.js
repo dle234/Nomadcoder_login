2bbbca23bdc8e5f4ec6e26bb79c08985019d491e
Kayeon <kky1373@naver.com>
2022-12-06 10:19:47 +0900

    Events part Third

--- web.js
+++ web.js
@@ -1,23 +1,36 @@
 
-const loginForm= document.querySelector(".loginForm");
-const loginInput =document.querySelector(".loginForm input");
+const loginForm= document.querySelector("#loginForm");
+const loginInput= document.querySelector("#loginForm input");
+const greeting =document.querySelector("#greeting");
+const HIDDEN_CLASSNAME="hidden"; //보통 문자열은 대문자로 선언
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+function paintGreeting(username){  // 헬로 보이게하기.
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
+}
 
 function onLoginSubmit(event){
     event.preventDefault(); 
-    // 어떤 event 의 기본 행동이든지 발생되지 않도록 막음
-    // 기본적으로 submit 할때마다 발동되는 새로고침을 막음.
-    console.log(loginInput.value);
-    
+    const username= loginInput.value; // 폼에 적은내용 유저네임에 저장
+    localStorage.setItem(USERNAME_KEY,username); //로컬저장소에 키 유저네임에 유저네임저장
+    //greeting.innerText= "Hello"+ username;
+    paintGreeting(username);  
+    loginForm.classList.add(HIDDEN_CLASSNAME);
 }
-loginForm.addEventListener("submit",onLoginSubmit);
-/////////////////////
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event) // 사용자가 클릭한 부분에 대한 정보를 알려줌(x축, y축 , html...)
+
+if(savedUsername === null){ // 널일때(아무것도 안했을때)
+    loginForm.classList.remove(HIDDEN_CLASSNAME); //로그인폼 보이게
+    loginForm.addEventListener("submit",onLoginSubmit); // 저장했을때 유저저장
+
+}else{
+    paintGreeting(savedUsername); //새로고침하면 핼로보임.
 }
-const link= document.querySelector("a");
-link.addEventListener("click",handleLinkClick)
-// ok 누르기 전까지는 링크로 이동 X(alert 사용하면 브라우저 모든 행동이 창 끌때까지 멈춤)
-// 뭐가 클릭 됐는지 정보 알고 싶음
+
+//null일 때(처음) form 화면 보임
+//유저네임 입력했을 때 로컬저장소에 올라가고 greeting Hello표시
+// 새로고침했을 때 유저네임에 그대로 있고 폼화면 사라짐. 
+
